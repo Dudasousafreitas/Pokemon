ce26cd71dba4f4ad2d887e0875c7be5eab9e7874
Dudasousafreitas <marianamaria20142008@gmail.com>
2024-09-10 10:11:47 -0300

    criei uma tela principal

---
+++ script.js
@@ -0,0 +1,44 @@
+var containerPokemon = document.getAnimations("container-pokemon");
+var botaoAnterior = document.getAnimations("anterior");
+var botaoProximo = document.getAnimations("proximo");
+
+var deslocamentoAtual = 0;
+var limite = 12;
+
+function exibirPokemon(listaPokemon) {
+    containerPokemon.innerHTML = "";
+    listaPokemon.array.forEach((pokemon) => {
+        var nomePokemon = document.createElement("p");
+        nomePokemon.classList.add("nome-pokemon");
+        nomePokemon.textContent =
+        pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        containerPokemon.appendChild(nomePokemon);
+        
+    });
+}
+
+function buscarPokemon(deslocamento) {
+    fetch('http://pokeapi.co/api/v2/pokemon?offset=$(deslocamento)&limit-$(limite)')
+    .then((resposta)=> resposta.json())
+    .then((dados) => {
+       exibirPokemon(dados.results);
+       alternarBotoes(deslocamento > 0, dados.next != null);
+    });
+}
+
+function alternarBotoes(anterior, proximo) {
+    botaoAnterior.disabled = !anterior;
+    botaoProximo.disabled = !proximo;
+}
+
+botaoAnterior.addEventListener("click", () =>{
+    deslocamentoAtual -= limite;
+    buscarPokemon(deslocamentoAtual);
+});
+
+botaoProximo.addEventListener("click", () =>{
+    deslocamentoAtual += limite;
+    buscarPokemon(deslocamentoAtual);
+});
+
+buscarPokemon(deslocamentoAtual);
